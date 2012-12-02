486c72413cc831cd5e71f9bf7d73b78645c4f433
tlk <jeremieboutoille@gmail.com>
2012-12-02 20:17:12 +0100

    Configure : ajout bouton et attributs dans l'activité

--- src/boutoille/iut/robot/ConfigureActivity.java
+++ src/boutoille/iut/robot/ConfigureActivity.java
@@ -2,14 +2,24 @@ package boutoille.iut.robot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ConfigureActivity extends Activity {
 
+	EditText mac = null;
+	EditText moteur = null;
+	Button valider = null;
+	
 	@Override
 	protected void onCreate(Bundle saveInstanceState)
 	{
 		super.onCreate(saveInstanceState);
 		setContentView(R.layout.activity_configure);
+		
+		mac = (EditText) findViewById(R.id.config_mac);
+		moteur = (EditText) findViewById(R.id.config_moteur);
+		valider = (Button) findViewById(R.id.config_valider);
 	}
 	
 }
